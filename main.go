0d27da505655381166be2d74fb7f7c6dc19c6ca9
Connor van Spronssen <info@dobefu.com>
2025-05-31 17:25:37 +0200

    feat: Add logic to type a string of text to a connected device

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"machine/usb"
+	"machine/usb/hid/keyboard"
+	"time"
 )
 
 func init() {
@@ -12,4 +14,8 @@ func init() {
 }
 
 func main() {
+	kb := keyboard.Port()
+	time.Sleep(time.Second * 2)
+
+	_, _ = kb.Write([]byte("Test"))
 }
